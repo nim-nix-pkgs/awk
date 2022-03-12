5a22aaa39a2f6f06fea1ae88ca82cecaf2bfc90d
hugosenari <hugosenari@gmail.com>
2022-03-12 00:40:13 -0300

    chore: re index awk

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim for awk programmers'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."awk-master".url = "path:./master";
+  inputs."awk-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."awk-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Nim for awk programmers'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-awk-master.flake = false;
+  inputs.src-awk-master.owner = "greencardamom";
+  inputs.src-awk-master.ref   = "refs/heads/master";
+  inputs.src-awk-master.repo  = "awk";
+  inputs.src-awk-master.type  = "github";
+  
+  inputs."nre".dir   = "nimpkgs/n/nre";
+  inputs."nre".owner = "riinr";
+  inputs."nre".ref   = "flake-pinning";
+  inputs."nre".repo  = "flake-nimble";
+  inputs."nre".type  = "github";
+  inputs."nre".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nre".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-awk-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-awk-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
