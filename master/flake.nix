19333637ff5bf26e3694c431f14ea4a87c477843
hugosenari <hugosenari@gmail.com>
2022-07-21 22:17:38 -0300

    chore: re index awk

---
+++ master/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = ''A library of awk functions in nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-awk-master.flake = false;
+  inputs.src-awk-master.ref   = "refs/heads/master";
+  inputs.src-awk-master.owner = "greencardamom";
+  inputs.src-awk-master.repo  = "awk";
+  inputs.src-awk-master.type  = "github";
+  
+  inputs."nre".owner = "nim-nix-pkgs";
+  inputs."nre".ref   = "master";
+  inputs."nre".repo  = "nre";
+  inputs."nre".dir   = "2_0_2";
+  inputs."nre".type  = "github";
+  inputs."nre".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nre".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-awk-master"];
+    over = if builtins.pathExists ./override.nix 
+           then { override = import ./override.nix; }
+           else { };
+  in lib.mkRefOutput (over // {
+    inherit self nixpkgs ;
+    src  = deps."src-awk-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  } );
+}
